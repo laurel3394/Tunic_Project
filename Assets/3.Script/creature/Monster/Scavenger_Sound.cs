33cc3316d5f8ea1a71d18a3ba8035e54a5388241
laurel <b4comes@gmail.com>
2024-01-09 20:45:31 +0900

    [feat] 보스 사운드 파일

---
+++ Assets/3.Script/creature/Monster/Scavenger_Sound.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scavenger_Sound : MonoBehaviour
+{
+    private void Boss_Walk()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.FoxWalk);
+    }
+    private void Boss_JumpDown()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.FoxWalk);
+    }
+    private void Boss_Ice()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.FoxWalk);
+    }
+    private void Boss_FireBall()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.FoxWalk);
+    }
+    private void Boss_Attack()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.FoxWalk);
+    }
+    private void Boss_Hit()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.BeefBoy_Hit);
+    }
+    private void Boss_()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.Sfx.FoxWalk);
+    }
+}
